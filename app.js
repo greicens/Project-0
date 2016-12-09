4b0428655e537dbf0fbbfd47235d0d3a2e6e94de
greicens <gsilva@mail.ccsf.edu>
2016-12-09 00:20:47 -0800

    swimmers are able to stop on the edge of pool

--- app.js
+++ app.js
@@ -3,20 +3,32 @@ $(document).ready(function() {
   console.log("document is ready");
   $("#swimmer1").on("click", moveRigth);
   $("#swimmer2").on("click", moveRigth);
-});
-// var scrollTop     = $(window).scrollTop(),
-//     elementOffset = $('#bigDot').offset().top,
-//     distance      = (elementOffset - scrollTop);
 
+
+});
 
 function moveRigth(event){
   event.preventDefault();
 
-    $("#swimmer1").animate({
-        marginLeft: "+=50px",
-    }, 1000 );
-    $("#swimmer2").animate({
-        marginLeft: "+=50px",
-    }, 1000 );
+  var scrollRight   = $(window).width(),
+  elementOffset1 = $('#swimmer1').offset(),
+  elementOffset2 = $('#swimmer2').offset();
+  console.log(elementOffset2);
 
+  console.log("inside moveRigth")
+  if(elementOffset1.left < 896.5){
+    console.log("inside if")
+    $("#swimmer1").animate({
+        marginLeft: "+=35px",
+    }, 1000 );
+  }else{
+    $( "#swimmer1" ).click(function() {
+    $( "#swimmer1" ).stop();
+    });
+  }
+  if(elementOffset2.left < 896.5){
+    $("#swimmer2").animate({
+        marginLeft: "+=40px",
+    }, 1000 );
+  }
 }
