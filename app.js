74a73c42af8f82836677d2cb6724e4f0b3e42095
greicens <gsilva@mail.ccsf.edu>
2016-12-11 00:26:24 -0800

    changed the boostrap grid

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 console.log("JS is linked")
 
-
 $(document).ready(function() {
   console.log("document is ready");
+  $("#play-button").on("click", startGame);
   $(window).on("keydown", moveRigth);
-  // $(window).on("keypress", stopWatch);
+
+//  $(window).on("keypress", stopWatch);
 
 
 });
@@ -14,46 +15,81 @@ $(document).ready(function() {
 //swimmer2 uses the A key
 function moveRigth(event){
   var scrollRight   = $(window).width(),
-  elementOffset1 = $('#swimmer1').offset(),
-  elementOffset2 = $('#swimmer2').offset();
-  console.log(elementOffset2);
+      elementOffset1 = $('#swimmer1').offset(),
+      elementOffset2 = $('#swimmer2').offset();
 
+    //if letter L is pressed the animation starts for swimmer1
+    //animation stops if swimmer1 offset is 1050
     if(event.keyCode === 76){
       if(elementOffset1.left < 1050){
-        console.log("inside if")
         $("#swimmer1").animate({
           marginLeft: "+=35px",
-        }, 1000 );
+        }, 500 );
       }else{
         $( "#swimmer1" ).keydown(function() {
+          console.log("swimmer1 stopped");
           $( "#swimmer1" ).stop();
+          console.log("swimmer1 stopped");
+          var stopTime1 = Date.now();
+          stopGame(stopTime1);
+          console.log(stopTime, " this is the stopTime for swimmer 2");
+
         });
       }
     }
 
+    //if letter L is pressed the animation starts for swimmer1
+    //animation stops if swimmer1 offset is 1050
     if(event.keyCode === 65){
       if(elementOffset2.left < 1050){
         $("#swimmer2").animate({
           marginLeft: "+=40px",
-        }, 1000 );
+        }, 500);
       }else{
         $( "#swimmer2" ).keydown(function() {
           $( "#swimmer2" ).stop();
+          var stopTime2 = Date.now();
+          stopGame(stopTime2);
+          console.log(stopTime, " this is the stopTime for swimmer 2");
         });
       }
     }
   }
 
+function refreshPool(){
+  location.reload();
+}
 
-// keeps track of time and display time on screen
-function stopWatch(event){
+function startGame(){
+  var startTime = Date.now(),
+      endTime,
+      counting = false;
+  console.log(startTime, " This is the time after play button is clicked");
 
+
+}
+
+// callback function used with setInterval
+// calculates and displays time so far
+// WITHOUT updating total
+function showCurrentTotal(){
+  // calculate time since we last pressed start
+  var elapsedTime = (Date.now() - startTime)/1000;
+  // display the total time so far
+  $(".message").text( (total + elapsedTime) + " seconds" );
 }
 
 //sets up a new game
 function Game(swimmer1, swimmer2){
+  //console.log("a new game has started!");
   this.swimmer1 = swimmer1;
   this.swimmer2 = swimmer2;
+}
+
+
+function stopGame(time){
+  console.log("inside stopGame");
+
 }
 //prototype for Game (gameStart and gameStop);
 //prototype for isWinner?
